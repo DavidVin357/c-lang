a67a5a7d66a824c95e01ea5fc7366fab1de43fd6
joonmun <71532043+joonmun@users.noreply.github.com>
2023-03-28 12:55:38 +0800

    committing before changing switch statements

--- src/runner/run.ts
+++ src/runner/run.ts
@@ -3,9 +3,21 @@ import { parse } from '../parser/parser'
 import * as cTree from '../cTree'
 import {runInterpreter} from "./runner";
 
-const code = 'const unsigned int x = 6; if (x == 5) { x = x + 5;} else {x = x - 5;} x;'
+const code = `
+int num = 10;
+switch (num) {
+    case 7:
+        num += 7;
+    case 10:
+        num += 10;
+    default:
+        num -= 10;  
+}
+num;`
+
 //if (3 + 2 == 5) int x = 5; x;
 //if (3 + 2 == 5) {int x = 5;} x;
 //const unsigned int x = 5; if (x == 5) { x = x + 5;} else {x = x - 5;} x;
+//const unsigned int x = 6; if (x == 5) { x = x + 5;} else {x = x - 5;} x;
 const res = runInterpreter(code)
 console.log(res)
