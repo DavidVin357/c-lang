5b9f4a0e4454ac9fddd05059823e83152b1b60a6
david <davydviniarskyi.dev@gmail.com>
2023-04-02 18:14:09 +0800

    apply gitignore

--- additional/test.js
+++
@@ -1,8 +0,0 @@
-// const megabyte = 2 ** 20
-
-// const buffer = new ArrayBuffer(20 * megabyte)
-
-// const MEMORY = new DataView(buffer)
-
-// MEMORY.setInt32(0, 4.6)
-// console.log(MEMORY.getInt32(0))
